57d47a341c935ade974ab41800e811611bcceb75
Matthias Perktold <7334069+mperktold@users.noreply.github.com>
2024-12-19 07:44:59 +0100

    Initial commit

---
+++ src/main/java/it/prodata/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package it.prodata.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/it/prodata/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package it.prodata.services;
+
+import org.springframework.lang.NonNullApi;
